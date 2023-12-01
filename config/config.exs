639fa84822d3b2892da18f14bf455c1b571cc79b
Kevin Tayah <ktayah@yahoo.com>
2023-12-01 14:23:40 -0500

    Add a pesky _ to the env key

--- config/config.exs
+++ config/config.exs
@@ -2,5 +2,5 @@ import Config
 
 config :alpaca_investors, AlpacaInvestors,
   base_url: "https://broker-api.sandbox.alpaca.markets",
-  key: System.get_env("BELAYALPACA__ALPACA_KEY", "key"),
-  secret: System.get_env("BELAYALPACA__ALPACA_SECRET", "secret")
+  key: System.get_env("BELAYALPACA__ALPACA__KEY", "key"),
+  secret: System.get_env("BELAYALPACA__ALPACA__SECRET", "secret")
