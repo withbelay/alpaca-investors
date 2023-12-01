ee43770ed8f1bde9ff0da963a853a9afb3c4582d
Kevin Tayah <ktayah@yahoo.com>
2023-12-01 14:03:42 -0500

    Allow envs to be used in config for task

--- config/config.exs
+++ config/config.exs
@@ -2,5 +2,5 @@ import Config
 
 config :alpaca_investors, AlpacaInvestors,
   base_url: "https://broker-api.sandbox.alpaca.markets",
-  key: "key",
-  secret: "secret"
+  key: System.get_env("BELAYALPACA__ALPACA_KEY", "key"),
+  secret: System.get_env("BELAYALPACA__ALPACA_SECRET", "secret")
